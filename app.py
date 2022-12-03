56c2a862447a6ecfbf41cdaf196cab5dee0d2e7f
= <=>
2022-12-04 00:46:54 +0100

    making progress

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer, nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('dashboard/home.html')
 
 @app.route('/about/<username>')
 def about_page(username):
@@ -16,4 +29,4 @@ def market_page():
     {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
     {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
 ]
-    return render_template('market.html', **locals())
+    return render_template('dashboard/market.html', **locals())
