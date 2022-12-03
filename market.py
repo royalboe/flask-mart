b22d8aa9b13e47be28d369976ca3c11f616b542f
= <=>
2022-12-03 21:08:57 +0100

    new commits

--- market.py
+++ market.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/about/<username>')
+def about_page(username):
+    return f'The About Page for {username}'
