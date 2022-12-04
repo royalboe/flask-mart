c8f7282f006655bb001eeeff265e6deda37b65c0
= <=>
2022-12-04 16:45:40 +0100

    restructure project

---
+++ market/routes.py
@@ -0,0 +1,20 @@
+from app import app
+
+
+@app.route('/')
+def index():
+    return render_template('dashboard/home.html')
+
+@app.route('/about/<username>')
+def about_page(username):
+    return f'The About Page for {username}'
+
+@app.route('/market')
+def market_page():
+    items = Item.query.all()
+#     items = [
+#     {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+#     {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+#     {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+# ]
+    return render_template('dashboard/market.html', **locals())
