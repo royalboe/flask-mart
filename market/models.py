c50c984c68a37f95465d83cc92853952717bef64
Royalboe <auomidejohnson@gmail.com>
2022-12-04 23:58:03 +0100

    user model

--- market/models.py
+++ market/models.py
@@ -1,5 +1,17 @@
 from market import db
 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email_address = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(60), nullable=False)
+    budget = db.Column(db.Integer, nullable=False, default=1000)
+    items = db.relationship('Item', backref='owned_user', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email_address}', '{self.budget}')"
+
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
