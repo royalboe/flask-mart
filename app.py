98edd8e5c8dbf23f39c1cb8f07599257cd658fba
= <=>
2022-12-04 17:06:19 +0100

    fixing bugss

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, url_for, redirect
-from flask_sqlalchemy import SQLAlchemy 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-db = SQLAlchemy(app)
+from market import app
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
