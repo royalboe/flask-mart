2a9306dffd659090df4dafe93556e5b9f4107678
Royalboe <auomidejohnson@gmail.com>
2022-12-05 15:25:34 +0100

    fixed register page ui

--- market/forms.py
+++ market/forms.py
@@ -6,15 +6,15 @@ from market.models import User
 
 
 class RegisterForm(FlaskForm):
-    # def validate_username(self, username_to_check):
-    #     user = User.query.filter_by(username=username_to_check.data).first()
-    #     if user:
-    #         raise ValidationError('Username already exists! Please try a different username.')
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('Username already exists! Please try a different username.')
 
-    # def validate_email_address(self, email_address_to_check):
-    #     email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
-    #     if email_address:
-    #         raise ValidationError('Email address already exists! Please try a different email address.')
+    def validate_email_address(self, email_address_to_check):
+        email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('Email address already exists! Please try a different email address.')
 
     username = StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()])
     email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
